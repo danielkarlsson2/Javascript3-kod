e8c4ab97c56a480f17720332ea0c34197f46281f
danielkarlsson2 <daniel@dkmusic.se>
2022-04-04 23:19:40 +0200

    traversy

--- lektioner/traversy/react-task-tracker/src/App.js
+++ lektioner/traversy/react-task-tracker/src/App.js
@@ -1,11 +1,47 @@
+import { useState } from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() { 
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Rasta hunden',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Bokföra',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Bygga altan',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
+
+// Delete task 
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder 
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => task.id === id ? 
+    { ...task, reminder: !task.reminder } : task))
+}
 
   return (
     <div className="container">
       <Header />
-      
+      {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask} 
+      onToggle={toggleReminder}
+      /> ) : ('No tasks to show')}
     </div>
   );
 }
